fbd2d88261f74d9c6042aff6f34bc61f93705a99
Oscar <34869@ma-web.nl>
2022-10-20 13:52:55 +0200

    added files

---
+++ README.md
@@ -0,0 +1,31 @@
+# Discord Status Changer (DSC)
+This is a tool i wrote which cycles your discord status. <br>
+You can enter files, lines or even directories and DSC will parse all the lines for you.
+
+# Installation & usage
+### Requirements
+- Python 3.10
+- All the modules needed
+   - Can be installed using `pip install -r requirements.txt`
+- Your discord token (<a href="https://youtube.com/watch?v=b1SY4zTNnAE">tutorial here</a>)
+
+### Installation
+- Clone the repository
+   - `git clone https://github.com/OscarThePythonWizz/DSC`
+- Install python modules
+   - `pip install requirements.txt`
+- Enter your token in `token.txt` (create the file if it doesn't exist)
+- Run the python file, and give it a file or directory as argument (for example: `python3 main.py mycoolstatuslines.txt` or `directory_full of_status_files`)
+
+# License
+```
+This project is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+
+This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this code. 
+If not, see <https://www.gnu.org/licenses/>. 
+```
---
+++ main.py
@@ -0,0 +1,125 @@
+import requests, json, sys, time
+from os.path import exists, isdir, isfile, join
+from os import walk
+from random import uniform
+from colorama import Fore, init
+
+init(autoreset=True)
+
+if exists('token.txt'):
+    print(f'{Fore.GREEN}+{Fore.RESET} Token found')
+    with open('token.txt') as fd:
+        token = fd.read().rstrip()
+else:
+    sys.exit(f'{Fore.RED}-{Fore.RESET} Token not found, please create a file named "token.txt" and put your discord token in there')
+
+# creates a session, speeds up performance and re-uses connections
+session = requests.Session()
+
+def clear() -> None:
+    """
+    clear() -> nothing
+
+    Clears the screen, thats it
+
+    :returns None: Nothing
+    """
+
+    print('\033c', end='')
+
+def change(status_msg: str) -> bool:
+    """
+    change(new status message) -> status
+
+    Changes the status of the user
+
+    :param status_msg str: new status message
+    :returns bool: True if no errors occurred, False if otherwise
+    """
+
+    body = json.dumps({
+        "custom_status": {
+            "text": status_msg
+        }
+    })
+
+    try:
+        req = session.patch(
+            'https://discordapp.com/api/v9/users/@me/settings',
+            headers={
+                "Authorization": token, 
+                "Content-Type": "application/json"
+            },
+            data=body
+        )
+
+        return req.status_code == 200
+    except Exception:
+        return False
+
+def parse(arg: str) -> list:
+    """
+    parse(file, lines or directory) -> lines
+
+    Parses all lines from the command line
+
+    :param arg str: File, folder or single string
+    :returns list: List of lines
+    """
+
+    lines = []
+    if exists(arg):
+        if isdir(arg):
+            print(f'{Fore.GREEN}{Fore.GREEN}+{Fore.RESET}{Fore.RESET} Parsing directory: {arg}')
+
+            for root, _, files in walk(arg):
+                for file in files:
+                    fpath = join(root, file)
+
+                    print(f'  {Fore.GREEN}+{Fore.RESET} Parsing file: {file}')
+                    with open(fpath) as fd:
+                        [lines.append(line.rstrip()) for line in fd.readlines()]
+        
+        elif isfile(arg):
+            print(f'{Fore.GREEN}+{Fore.RESET} Parsing file: {arg}')
+            with open(arg) as fd:
+                [lines.append(line.rstrip()) for line in fd.readlines()]
+    else:
+        print(f'{Fore.GREEN}+{Fore.RESET} Parsing lines')
+        [lines.append(x) for x in arg.split('|')]
+    
+    return lines
+
+if __name__ == '__main__':
+    clear()
+
+    args = sys.argv[1:]
+    running = True
+
+    # parse arguments
+    lines = []
+    for arg in args:
+        lines += parse(arg)
+    
+    print(f'{Fore.GREEN}+{Fore.RESET} Loaded {str(len(lines))} lines.\n')
+    while running:
+        for line in lines:
+            try:
+                status = change(line)
+
+                if status:
+                    print(f'{Fore.GREEN}+{Fore.RESET} Status changed: {line}')
+
+                else:
+                    print(f'{Fore.RED}-{Fore.RESET} Failed to change status to: {line}')
+
+                time.sleep(uniform(3, 7))
+
+            except KeyboardInterrupt:
+                running = False; break
+            
+            except Exception as e:
+                print(f'{Fore.RED}-{Fore.RESET} Exception occurred: {str(e).rstrip()}')
+                running = False; break
+    
+    print('')
